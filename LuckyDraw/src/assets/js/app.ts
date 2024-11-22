61251a985166d0d7536c00c2e251914728ec695a
Jolyn Wong <jolynw03@gmail.com>
2024-11-22 11:22:51 +0800

    increased timing

--- LuckyDraw/src/assets/js/app.ts
+++ LuckyDraw/src/assets/js/app.ts
@@ -42,7 +42,7 @@ import SoundEffects from '@js/SoundEffects';
   }
 
   const soundEffects = new SoundEffects();
-  const MAX_REEL_ITEMS = 40;
+  const MAX_REEL_ITEMS = 50;
   const CONFETTI_COLORS = ['#26ccff', '#a25afd', '#ff5e7e', '#88ff5a', '#fcff42', '#ffa62d', '#ff36ff'];
   let confettiAnimationId;
 
